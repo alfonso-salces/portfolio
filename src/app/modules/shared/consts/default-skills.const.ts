d3ec0931c7f3d81dbca742331b68120f20d4167e
Alfonso <alfonsomtb98@gmail.com>
2020-06-22 23:20:06 +0200

    Cambios en los textos

--- src/app/modules/shared/consts/default-skills.const.ts
+++ src/app/modules/shared/consts/default-skills.const.ts
@@ -1,19 +1,22 @@
 import { SkillModel } from '../../home/models/skill.model';
 
 export const defaultSkills = [
-    {
-        icon: 'responsive-light.svg',
-        title: 'Responsive',
-        description: 'Esto se supone que es una descripción. Aquí tendría que escribir mis habilidades o contarte un poco mi vida, pero de momento se queda así.'
-    },
-    {
-        icon: 'module-light.svg',
-        title: 'Modular',
-        description: 'Esto se supone que es una descripción. Aquí tendría que escribir mis habilidades o contarte un poco mi vida, pero de momento se queda así.'
-    },
-    {
-        icon: 'strategy-light.svg',
-        title: 'Estrategia',
-        description: 'Esto se supone que es una descripción. Aquí tendría que escribir mis habilidades o contarte un poco mi vida, pero de momento se queda así.'
-    }
+  {
+    icon: 'responsive-light.svg',
+    title: 'Responsive',
+    description:
+      'Los diseños que realizo son completamente responsive, adaptados a los últimos estándares de medida de los diferentes dispositivos existentes hoy día.',
+  },
+  {
+    icon: 'module-light.svg',
+    title: 'Modular',
+    description:
+      'Cada aplicación diseñada será modular, con lo cual, cada parte será reutilizable, tanto los módulos como sus respectivos componentes.',
+  },
+  {
+    icon: 'strategy-light.svg',
+    title: 'Planificación',
+    description:
+      'Soy consciente de que una planificación exhaustiva es el propulsor de cualquier proyecto exitoso, por lo tanto siempre planifico hasta el más mínimo detalle en los proyectos en los que participo.',
+  },
 ] as SkillModel[];
